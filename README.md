c5420d9c2d2c8f0dc9cbd4791135bd86b523288e
Tool Cool <toolcool.org@gmail.com>
2022-10-12 07:55:37 +0300

    config

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This is a markdown-based documentation generator. The module is used in the [Tool Cool Range Slider](https://github.com/toolcool-org/toolcool-range-slider) library.
 
+## Usage
+
+- First of all, edit the configuration file located in **/src/docs/data/config.json**.
+
 ## Commands
 
 The following command is used to compile the documentation website:
--- src/generator/render/macros-provider.js
+++ src/generator/render/macros-provider.js
@@ -1,8 +1,12 @@
 export const setMacros = (html, data) => {
   let result = html;
+
   result = result.replaceAll('{% css-hash %}', data.cssTimeStamp);
   result = result.replaceAll('{% js-hash %}', data.jsTimeStamp);
   result = result.replaceAll('{% website-name %}', data.mainConfig.website.name);
+  result = result.replaceAll('{% github %}', data.mainConfig.social.github);
+  result = result.replaceAll('{% twitter %}', data.mainConfig.social.twitter);
+  result = result.replaceAll('{% google-analytics-gtag %}', data.mainConfig.analytics.gtag);
 
   return result;
 };
