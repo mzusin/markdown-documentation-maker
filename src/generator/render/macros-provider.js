8f79ca4cb8fbb0fa3036cf4bb24ceac757785fb1
Tool Cool <toolcool.org@gmail.com>
2022-10-12 08:02:32 +0300

    config

--- src/generator/render/macros-provider.js
+++ src/generator/render/macros-provider.js
@@ -6,6 +6,7 @@ export const setMacros = (html, data) => {
   result = result.replaceAll('{% website-name %}', data.mainConfig.website.name);
   result = result.replaceAll('{% github %}', data.mainConfig.social.github);
   result = result.replaceAll('{% twitter %}', data.mainConfig.social.twitter);
+  result = result.replaceAll('{% email %}', data.mainConfig.social.email);
   result = result.replaceAll('{% google-analytics-gtag %}', data.mainConfig.analytics.gtag);
 
   return result;
