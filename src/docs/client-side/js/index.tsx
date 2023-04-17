f0c526cc374870a9bc8c7d79c1b3beecf7303bc5
Miriam Zusin <miriamzusin@gmail.com>
2023-04-17 23:02:27 +0300

    fix js bugs and dark mode styles

--- src/docs/client-side/js/index.tsx
+++ src/docs/client-side/js/index.tsx
@@ -3,6 +3,9 @@ import { initMenuCollapsible, initMenuScroll } from './menu/side-menu';
 import { handleDarkLightModes } from './dark-mode';
 
 const init = () => {
+  const $special = document.getElementById('special-page');
+  if($special) return;
+
   initMobileMenu();
   initMenuScroll();
   initMenuCollapsible();
