3e8dfc2d3d39dead8d817c430a9212b7f96c7234
Miriam Zusin <miriamzusin@gmail.com>
2023-04-17 22:25:20 +0300

    remove config entries

--- src/docs/generator/render/macros-provider.js
+++ src/docs/generator/render/macros-provider.js
@@ -5,9 +5,7 @@ export const setMacros = (html, data) => {
   result = result.replaceAll('{% js-hash %}', data.jsTimeStamp);
   result = result.replaceAll('{% website-name %}', data.mainConfig.website.name);
   result = result.replaceAll('{% github %}', data.mainConfig.social.github);
-  result = result.replaceAll('{% twitter %}', data.mainConfig.social.twitter);
   result = result.replaceAll('{% email %}', data.mainConfig.social.email);
-  result = result.replaceAll('{% google-analytics-gtag %}', data.mainConfig.analytics.gtag);
 
   return result;
 };
