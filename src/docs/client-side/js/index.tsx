fbc9600b518e11d3c730adedd764460b2e48c026
Miriam Zusin <miriamzusin@gmail.com>
2023-04-25 05:11:38 +0300

    import highlight.js

--- src/docs/client-side/js/index.tsx
+++ src/docs/client-side/js/index.tsx
@@ -1,8 +1,13 @@
 import { initMobileMenu } from './menu/mobile-menu';
 import { initMenuCollapsible, initMenuScroll } from './menu/side-menu';
 import { handleDarkLightModes } from './dark-mode';
+import hljs from 'highlight.js';
 
 const init = () => {
+  if(hljs){
+    hljs.highlightAll();
+  }
+
   const $special = document.getElementById('special-page');
   if($special) return;
 
